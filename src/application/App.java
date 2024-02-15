db94a9e77a16ac365b4961f701b541944f80272b
Bruno Matheus Ribeiro <brunomatheus168@gmail.com>
2024-02-15 15:23:43 -0300

    "Starting to implement Board and Piece"

--- src/application/App.java
+++ src/application/App.java
@@ -1,12 +1,12 @@
 package application;
 
+import application.boardgame.Board;
 import application.boardgame.Position;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
         
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
     }
 }
